85263050c545fd58e14e1ccf08b703edd6d019fe
dev-muhammadabid <muhammadabid.dev@gmail.com>
2024-07-26 23:17:56 +0530

    FIXES

--- app/src/main/java/com/example/lockapp/LockActivity.java
+++ app/src/main/java/com/example/lockapp/LockActivity.java
@@ -1,7 +1,10 @@
 package com.example.lockapp;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -12,8 +15,25 @@ public class LockActivity extends Activity {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         String packageName = intent.getStringExtra("package_name");
-        // Add logic to lock the app with the given package name
-        Toast.makeText(this, "App locked: " + packageName, Toast.LENGTH_SHORT).show();
+
+        PackageManager packageManager = getPackageManager();
+        try {
+            ApplicationInfo appInfo = packageManager.getApplicationInfo(packageName, 0);
+            if (appInfo != null) {
+                Intent launchIntent = packageManager.getLaunchIntentForPackage(packageName);
+                if (launchIntent != null) {
+                    ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+                    activityManager.killBackgroundProcesses(packageName);
+                    Toast.makeText(this, "App locked: " + packageName, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Unable to lock app: " + packageName, Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "App not found: " + packageName, Toast.LENGTH_SHORT).show();
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(this, "App not found: " + packageName, Toast.LENGTH_SHORT).show();
+        }
         finish();
     }
 }
--- app/src/main/java/com/example/lockapp/UnlockActivity.java
+++ app/src/main/java/com/example/lockapp/UnlockActivity.java
@@ -2,6 +2,8 @@ package com.example.lockapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -12,8 +14,24 @@ public class UnlockActivity extends Activity {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         String packageName = intent.getStringExtra("package_name");
-        // Add logic to unlock the app with the given package name
-        Toast.makeText(this, "App unlocked: " + packageName, Toast.LENGTH_SHORT).show();
+
+        PackageManager packageManager = getPackageManager();
+        try {
+            ApplicationInfo appInfo = packageManager.getApplicationInfo(packageName, 0);
+            if (appInfo != null) {
+                Intent launchIntent = packageManager.getLaunchIntentForPackage(packageName);
+                if (launchIntent != null) {
+//                    startActivity(launchIntent);
+                    Toast.makeText(this, "App unlocked: " + packageName, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Unable to unlock app: " + packageName, Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "App not found: " + packageName, Toast.LENGTH_SHORT).show();
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(this, "App not found: " + packageName, Toast.LENGTH_SHORT).show();
+        }
         finish();
     }
 }
