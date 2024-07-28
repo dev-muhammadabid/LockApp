5d1e2f5738c99fc29b223b35bf38a0e6def609e9
dev-muhammadabid <muhammadabid.dev@gmail.com>
2024-07-28 18:12:44 +0530

    FEW BUGS

--- app/src/main/java/com/example/lockapp/LockActivity.java
+++ app/src/main/java/com/example/lockapp/LockActivity.java
@@ -1,39 +1,80 @@
 package com.example.lockapp;
 
 import android.app.Activity;
-import android.app.ActivityManager;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class LockActivity extends Activity {
 
+    private static final int REQUEST_CODE_UNLOCK = 2;
+    private String packageName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        String packageName = intent.getStringExtra("package_name");
+        setContentView(R.layout.app_list_item);
 
+        Intent intent = getIntent();
+        packageName = intent.getStringExtra("package_name");
+
+        if (packageName == null) {
+            Toast.makeText(this, "Package name is missing", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        lockApp(packageName);
+    }
+
+    private void lockApp(String packageName) {
         PackageManager packageManager = getPackageManager();
         try {
             ApplicationInfo appInfo = packageManager.getApplicationInfo(packageName, 0);
             if (appInfo != null) {
-                Intent launchIntent = packageManager.getLaunchIntentForPackage(packageName);
-                if (launchIntent != null) {
-                    ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-                    activityManager.killBackgroundProcesses(packageName);
-                    Toast.makeText(this, "App locked: " + packageName, Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, "Unable to lock app: " + packageName, Toast.LENGTH_SHORT).show();
-                }
+                Log.d("LockActivity", "App found: " + packageName);
+                showLockScreen();
             } else {
+                Log.e("LockActivity", "App not found: " + packageName);
                 Toast.makeText(this, "App not found: " + packageName, Toast.LENGTH_SHORT).show();
+                finish();
             }
         } catch (PackageManager.NameNotFoundException e) {
+            Log.e("LockActivity", "App not found exception: " + e.getMessage());
             Toast.makeText(this, "App not found: " + packageName, Toast.LENGTH_SHORT).show();
+            finish();
         }
-        finish();
+    }
+
+    private void showLockScreen() {
+        Intent intent = new Intent(this, LockScreenActivity.class);
+        intent.putExtra("package_name", packageName);
+        startActivityForResult(intent, REQUEST_CODE_UNLOCK);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_UNLOCK) {
+            if (resultCode == RESULT_OK) {
+                launchApp();
+            } else {
+                Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show();
+            }
+            finish();
+        }
+    }
+
+    private void launchApp() {
+        PackageManager packageManager = getPackageManager();
+        Intent intent = packageManager.getLaunchIntentForPackage(packageName);
+        if (intent != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Unable to launch app: " + packageName, Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/example/lockapp/LockScreenActivity.java
@@ -0,0 +1,74 @@
+package com.example.lockapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import java.util.concurrent.Executor;
+
+public class LockScreenActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_screen);
+
+        authenticateUser();
+    }
+
+    private void authenticateUser() {
+        BiometricManager biometricManager = BiometricManager.from(this);
+        int canAuthenticate = biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG | BiometricManager.Authenticators.DEVICE_CREDENTIAL);
+        Log.d("LockScreenActivity", "canAuthenticate result: " + canAuthenticate);
+        if (canAuthenticate == BiometricManager.BIOMETRIC_SUCCESS) {
+            Executor executor = ContextCompat.getMainExecutor(this);
+            BiometricPrompt.AuthenticationCallback callback = new BiometricPrompt.AuthenticationCallback() {
+                @Override
+                public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                    super.onAuthenticationError(errorCode, errString);
+                    Log.e("LockScreenActivity", "Authentication error: " + errString);
+                    setResult(RESULT_CANCELED);
+                    finish();
+                }
+
+                @Override
+                public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                    super.onAuthenticationSucceeded(result);
+                    Log.d("LockScreenActivity", "Authentication succeeded");
+                    setResult(RESULT_OK);
+                    finish();
+                }
+
+                @Override
+                public void onAuthenticationFailed() {
+                    super.onAuthenticationFailed();
+                    Log.d("LockScreenActivity", "Authentication failed");
+                    Toast.makeText(LockScreenActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                }
+            };
+
+            BiometricPrompt biometricPrompt = new BiometricPrompt(this, executor, callback);
+
+            BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Biometric Authentication")
+                    .setSubtitle("Authenticate to unlock the app")
+                    .setDescription("Use your biometric credential to unlock this app.")
+                    .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG | BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+                    .build();
+
+            biometricPrompt.authenticate(promptInfo);
+        } else {
+            Log.e("LockScreenActivity", "Biometric authentication not supported: " + canAuthenticate);
+            Toast.makeText(this, "Biometric authentication not supported", Toast.LENGTH_SHORT).show();
+            setResult(RESULT_CANCELED);
+            finish();
+        }
+    }
+
+}
